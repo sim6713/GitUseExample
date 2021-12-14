5c1abcbe12f04a0d534be498e98636433568eee5
Aliaksei Simukou <simukov5@gmail.com>
2021-12-14 01:22:14 -0800

    Initial Commit

---
+++ Shared/GitUseExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitUseExampleApp.swift
+//  Shared
+//
+//  Created by Симуков Алексей on 14.12.2021.
+//
+
+import SwiftUI
+
+@main
+struct GitUseExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
